7452b03233d93500eb68a9d1da36552811bd75da
cqb13 <cqb13.main@gmail.com>
2022-09-04 18:42:45 -0400

    save load

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 const toggle = document.getElementById("toggle-switch");
 
-console.log("running?");
-
 toggle.addEventListener("change", function () {
   chrome.storage.sync.get("state", function (data) {
     var state = data.state;
@@ -16,13 +14,12 @@ toggle.addEventListener("change", function () {
 
 function startUp() {
   chrome.storage.sync.get("state", function (data) {
-    if (data.state == false) {
-      console.log("visible");
-    } else {
-      console.log(data.state);
-      console.log("not visible");
-    }
+    noCheck(data.state);
   });
+}
+
+function noCheck(status) {
+  toggle.checked = status;
 }
 
 startUp();
