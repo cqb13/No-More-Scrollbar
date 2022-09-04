67d3bcd431076d888f2ae33d12555c7b6ec25f27
cqb13 <cqb13.main@gmail.com>
2022-09-04 18:39:32 -0400

    fixed save issues

--- popup.js
+++ popup.js
@@ -1,21 +1,27 @@
 const toggle = document.getElementById("toggle-switch");
 
+console.log("running?");
+
 toggle.addEventListener("change", function () {
   chrome.storage.sync.get("state", function (data) {
     var state = data.state;
-    if (toggle.checked) {
+    if (state == false) {
       state = true;
-    } else if (!toggle.checked) {
+    } else if (state == true) {
       state = false;
     }
-    console.log(data.state + "why")
-    chrome.stroage.sync.set({ state: state });
+    chrome.storage.sync.set({ state: state });
   });
 });
 
 function startUp() {
   chrome.storage.sync.get("state", function (data) {
-    console.log(data.state);
+    if (data.state == false) {
+      console.log("visible");
+    } else {
+      console.log(data.state);
+      console.log("not visible");
+    }
   });
 }
 
