d4a714534a745c2f09b32216963acb6cafa66b76
cqb13 <cqb13.main@gmail.com>
2023-08-01 15:52:28 -0400

    Changed normal style

--- website-edits/poof.js
+++ website-edits/poof.js
@@ -4,10 +4,21 @@ var amount = 0;
 chrome.storage.sync.get(["stateNMS", "firstNMS"], function (data) {
   if (!data.stateNMS && data.firstNMS == true) {
     HideScrollbar();
+  } else {
+    ShowScrollbar();
   }
 });
 
 function HideScrollbar() {
   style.innerHTML = `*::-webkit-scrollbar {display: none;}`;
+  document.head.appendChild(style);
+}
+
+function ShowScrollbar() {
+  style.innerHTML = `
+    *::-webkit-scrollbar {width: 10px;}
+    *::-webkit-scrollbar-track {background: #00000000;}
+    *::-webkit-scrollbar-thumb {background: #496bbe4d;}
+    *::-webkit-scrollbar-thumb:hover {background: #496bbeb3;}`;
   document.head.appendChild(style);
 }
